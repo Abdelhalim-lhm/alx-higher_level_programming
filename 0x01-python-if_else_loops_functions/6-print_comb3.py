60ae701245d533c95b90f2ff371d04688e5964de
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-03 15:23:02 -0700

    6. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,14 +3,11 @@ for i in range(90):
     if i < 89:
         is_dup = False
         double_i = "{:02d}".format(i)
-        
         for j in range(i + 1, 100):
             double_j = "{:02d}".format(j)
-            
             if set(double_i) == set(double_j):
                 is_dup = True
-                break
-        
+                break 
         if is_dup:
             print("{:02d}, ".format(i), end='')
     else:
