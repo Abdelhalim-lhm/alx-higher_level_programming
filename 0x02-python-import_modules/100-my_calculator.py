c0c1916c298ed214e21db54d92045eba5856336d
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-05 14:23:38 -0700

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     n_arg = len(sys.argv)
     if n_arg != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(0)
+        sys.exit(1)
     else:
         a = int(sys.argv[1])
-        b = int(sys.argv[3])
         op = sys.argv[2]
+        b = int(sys.argv[3])
         if op == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
             sys.exit(0)
