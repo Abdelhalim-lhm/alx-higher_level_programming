2cfe9f78b1c0ff84293d26782f04cca4fc333f0e
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-05 12:34:01 -0700

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
+    h_names = dir(hidden_4)
+    for i in h_names:
         if i[:2] != "__":
             print(i)
