a325151533ce97b895201718782b00d511a0a0c7
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-09 11:32:17 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     lenght = len(sentence)
-    if length == 0:
+    if lenght == 0:
         F_letter = None
     else:
         F_letter = sentence[0]
