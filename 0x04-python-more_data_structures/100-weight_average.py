3e425faf74e94195a9266b24b662ab73f74826ba
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-11 13:58:42 -0700

    13. Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyton3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    else:
+        s = 0
+        f = 0
+        for (x, y) in my_list:
+            f += x * y
+            s += y
+        return f / s
