6c8631ef2c7fdbd42e218a490c95f8eeff5bb83a
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-29 14:12:49 -0700

    5. Max integer - Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """ Unittest class for max_integer """
+    def test_normal(self):
+        """ Unittest normal case """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_same(self):
+        """ Unittest same case """
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+
+    def test_no_args(self):
+        """ Unittest no args case """
+        self.assertEqual(max_integer(), None)
+
+    def test_empty(self):
+        """ Unittest empty case """
+        self.assertEqual(max_integer([]), None)
+
+    def test_negative(self):
+        """ Unittest negative case"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_string(self):
+        """ Unittest string case"""
+        with self.assertRaises(TypeError):
+            max_integer([1, "string", 2])
+
+    def test_not_list(self):
+        """ Unittest not a list case """
+        with self.assertRaises(TypeError):
+            max_integer(1.5)
+
+
+if __name__ == "__main__":
+    unittest.main()
