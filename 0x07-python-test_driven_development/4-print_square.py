0a27111f85c623e79ef23b43e353f7c235714dbf
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-29 08:35:49 -0700

    3. Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_square(size):
+    '''  function that prints a square with the character # '''
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.filetest("tests/4-print_square.txt")
