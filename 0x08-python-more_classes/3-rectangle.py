504c1db00ade5a4b81759fc0671399e5f638f04f
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-30 11:48:48 -0700

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,5 +52,6 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 str1 += "#"
-            str1 += "\n"
+            if i < self.__height - 1:
+                str1 += "\n"
         return str1
