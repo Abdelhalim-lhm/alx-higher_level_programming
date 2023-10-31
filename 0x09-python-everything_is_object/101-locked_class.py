e659e2fb639f85ad3756f1235e2f5efcad18c52a
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-10-31 03:36:02 -0700

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+''' Low memory cost code '''
+
+
 class LockedClass:
-    '''  prevents the user from dynamically"
-    "creating new instance attributes '''
+    '''
+    prevents the user from dynamically
+    creating new instance attributes
+    '''
+
     __slots__ = ["first_name"]
