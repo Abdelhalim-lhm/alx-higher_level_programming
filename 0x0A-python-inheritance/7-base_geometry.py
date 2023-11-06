8918b8eebf97ec203f3c2329b7851839f415cbbc
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-06 08:16:27 -0800

    7. Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
     def area(self):
         ''' Method area defintion '''
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         ''' integer_validator definition '''
         self.name = name
