1dc7aab36fd7c140fb8b5b1340ac71337f7a8da5
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-06 06:07:17 -0800

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' module def '''
+
+
+class MyList(list):
+    ''' Mylist class definition '''
+    def print_sorted(self):
+        ''' print function '''
+        print(sorted(self))
