ab713bffd4e1bf8a7245e545f0d10440b822defd
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-06 07:50:29 -0800

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' BaseGeometry module '''
+
+
+class BaseGeometry:
+    ''' BaseGeometry class definition '''
+    def area(self):
+        ''' Method area defintion '''
+        raise Exception("area() is not implemented")
