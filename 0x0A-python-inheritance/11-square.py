6539b72640773d923c51cbedb0e5d206f061a1eb
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-06 09:15:33 -0800

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' BaseGeometry importation module '''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' Sqaure class definition '''
+    def __init__(self, size):
+        ''' square definition '''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        ''' method aream definition '''
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
