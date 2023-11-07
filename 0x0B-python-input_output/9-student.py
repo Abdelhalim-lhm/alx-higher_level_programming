051db76e4c009bb563c1f9714a8e1020a709c3f3
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-07 12:47:29 -0800

    9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,10 +5,11 @@
 class Student:
     ''' class student definition '''
     def __init__(self, first_name, last_name, age):
-        ''' instance definition '''
+        ''' instance attribute definition '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         ''' json dict definition '''
         return self.__dict__
