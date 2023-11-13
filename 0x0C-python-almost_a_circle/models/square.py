0a31de7f0737e8a6c2922fce6bc55631d20ffc0e
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-13 11:16:09 -0800

    11. Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,12 @@ class Square(Rectangle):
     def __str__(self):
         ''' str representation '''
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
