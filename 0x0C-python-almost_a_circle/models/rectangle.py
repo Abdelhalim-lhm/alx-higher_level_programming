5a690208e15a75ac7d90d7cce8ced3aae8389d0b
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-11-13 08:56:58 -0800

    5. Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,3 +64,10 @@ class Rectangle(Base):
     def area(self):
         ''' Area of the rectangle '''
         return self.__width * self.__height
+
+    def display(self):
+        ''' print rectangle '''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
