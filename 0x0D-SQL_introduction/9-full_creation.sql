29f0fac4b5de59ac13d4048a1138a0188938c5b6
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2023-12-12 12:49:27 -0800

    9. Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Write a script that creates a table second_table in the database
+CREATE TABLE IF NOT EXISTS seconde_table (id INT,
+	name VARCHAR(256),
+	score INT);
+INSERT INTO seconde_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO seconde_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO seconde_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO seconde_table (id, name, score) VALUES (4, "Geoge", 8);
