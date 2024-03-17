d58725d69371af6360cf7c9b7bd889b2cd0bf832
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-03-17 16:38:23 -0700

    16. List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,5 +20,6 @@ if __name__ == "__main__":
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print("    ", end="")
+            print(city.id, city.name, sep=": ")
     session.close()
