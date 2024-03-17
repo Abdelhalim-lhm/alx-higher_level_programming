e6f901d44255f18d11171a62dfb2a30e10a455f1
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-03-17 15:57:18 -0700

    15. City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" imports definition """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    """ class City definition """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    states = relationship("State", back_populates="cities")
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" imports definition """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ class State definition """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='states')
