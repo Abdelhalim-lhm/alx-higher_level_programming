b8ec8f1318e254849f27e47985803c03f6e6a62f
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-03-18 12:48:22 -0700

    16. List relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -12,3 +12,4 @@ class City(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    states = relationship("State", back_populates="cities")
