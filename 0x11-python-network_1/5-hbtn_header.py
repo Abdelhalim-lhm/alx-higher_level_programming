9c1ee7687e63e736528b196fbac60d2f87de7aac
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-03-28 17:03:39 -0700

    5. Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" displays the value of the variable X-Request-Id in the response header """
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    the_id = r.headers["X-Request-Id"]
+    print(the_id)
